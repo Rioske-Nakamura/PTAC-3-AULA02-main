02b4334b75ff95fb0974b25257aee2e31fe9c44d
Rioske-Nakamura <131805559+Rioske-Nakamura@users.noreply.github.com>
2024-02-28 14:27:02 -0400

    1.3
    css

--- src/app/componente/menu.js
+++ src/app/componente/menu.js
@@ -4,14 +4,18 @@ import Image from "next/image";
 
 export default function Menu(){
     return(
-        <header className={styles.cabecalhonpm}>
-            <Image 
-            width={100}
-            height={100} 
-            src="https://www.ifms.edu.br/marcaifms.png"
-            ></Image>
-            <nav>
+        <header className={styles.cabecalho}>
+            
+            <nav className="navbar">
+
                 <ul>
+                    <li>
+                        <Image 
+                            width={100}
+                            height={100} 
+                            src="https://www.ifms.edu.br/marcaifms.png"
+                        ></Image>
+                    </li>
                     <Link href="/">
                         <li>Home</li>
                     </Link>
