e9fad525f536ec590582b88256866394b9760cb5
Rioske-Nakamura <131805559+Rioske-Nakamura@users.noreply.github.com>
2024-02-28 14:14:08 -0400

    1.2
    image

--- src/app/componente/menu.js
+++ src/app/componente/menu.js
@@ -1,8 +1,15 @@
-import Link from "next/link"
+import Link from "next/link";
+import styles from "./menu.modules.css";
+import Image from "next/image";
+
 export default function Menu(){
     return(
-        <header>
-            <h1>Menu</h1>
+        <header className={styles.cabecalhonpm}>
+            <Image 
+            width={100}
+            height={100} 
+            src="https://www.ifms.edu.br/marcaifms.png"
+            ></Image>
             <nav>
                 <ul>
                     <Link href="/">
