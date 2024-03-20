13f569c94188aa958df1505af5d1511cd163e769
Rioske-Nakamura <131805559+Rioske-Nakamura@users.noreply.github.com>
2024-03-20 14:15:16 -0400

    1.5
    bache

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,25 @@
+const url = "http://back-end-ifms.vercel.app.campi";
 
 export default async function Home() {
 
+
+
+  const resposta = await fetch(url,
+  {
+    next:{
+      revalidate:1
+    }
+  });
+  const campi = await resposta.json();
+ 
   return (
     <main>
       <h1>Home</h1>
+      {campi.map((campus) =>
+        <div>
+             <p>{campus.nome_campua}</p>
+        </div>
+      )}
     </main>
   )
 }
