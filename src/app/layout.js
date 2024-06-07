56d4fdbcada8937373f7fdd057b980c8766c6864
priscila <131805559+Rioske-Nakamura@users.noreply.github.com>
2024-06-07 09:37:56 -0400

    a

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,4 @@
-import Menu from "./componente/menu"
-import Footer from "./componente/rodape"
-import Localizacao from "./registro/localizacao"
+
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,10 +9,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
       <body>
-          <Menu/>
-          <Localizacao/>
+         
           {children}
-          <Footer/>
+       
         </body>
     </html>
   )
